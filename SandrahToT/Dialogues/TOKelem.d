c9b3607c103d4c072b427a357361c68476e9c845
Roxanne <cwillenbockel@aol.com>
2016-04-27 21:16:46 +0200

    V 2.01b

--- SandrahToT/Dialogues/TOKelem.d
+++ SandrahToT/Dialogues/TOKelem.d
@@ -1,3 +1,4 @@
+BEGIN CVGuntt
 BEGIN ToKelem
 IF~InParty(Myself) IsGabber("CVsandr")~THEN BEGIN Pid
 SAY~Midnight, my daystar and beloved?~
@@ -17,3 +18,38 @@ SAY~Midnight, he is our thief and useful and thus we keep him. He knows he is no
 IF~~THEN REPLY~(Sigh) May you stay right in that, Kelemvor.~EXIT
 IF~~THEN REPLY~I KNOW he will murder you, I know it, cannot you understand that!~DO~IncrementGlobal("Sanpoints","Global",-3) ScreenShake([1600.1604],5)Wait(2) ReallyForceSpellRES("TOSpint","CVSandr") ~EXIT
 END
+
+CHAIN
+IF~Global("ToKelemcurse","Global",1)~THEN CVGuntt Kelcur1
+~Kelemvor, the murderer of his father, my father, the black sheep of the family.~
+DO~SetGlobal("ToKelemcurse","Global",2)~
+==ToKelem~You have sunken low, how else is your presence in this company to be explained, Guntharr Lyonsbane. Call me black sheep of the family but I do not call you *brother*.~
+==CVGuntt~Bah - shall we write these *famous last words* on your gravestone, fool?~
+=~You have not changed, following the skirts of weak women has made you a murderer and now makes you a corpse.~
+==ToKelem~It were in deed weak women whom our father assaulted and killed until I ended his regime of terror.~
+==CVGuntt~He made us MEN, at least three of us, probably not his youngest who preferred the education of his mistresses.~
+==ToKelem~Enough - you followed his footsteps in life and now you follow him in death.~DO~ClearAllActions() StartCutSceneMode() StartCutScene("ToKeGut") ~EXIT
+
+CHAIN
+IF~Global("ToKelemcurse","Global",4)~THEN ToKelem Kelkur2
+~That...was unexpected...~
+DO~SetGlobal("ToKelemcurse","Global",5) ~
+==CVSan25J~It was, my dear - but are we both talking about the same thing.~
+==ToKelem~I owe you some explanation, my love.~
+=~To find one of my brothers here was both unexpected and again not. Our father drilled us to be mercenairies and fighters for any good profit.~
+==TOPellig~He at least tried to, obviously with different results among his sons.~
+==ToKelem~It was fate and the hands of some women which prevented me from being here instead of my brother.~
+==ToCyric~Has this *unexpected* shapeshifting-thingy anything to do with your story, my *pussycat*, hihihi?~
+==Tolini~Can't you see that it surprised Kelemvor as much as us, Cyric?~
+==ToKelem~It did and it did not. It is all connected with each other, this curse of the Lyonbanes.~
+==TOAdon~Nothing helps you better than telling your friends about it, Kelemvor.~
+==ToKelem~The curse of the Lyonsbanes had been passed down for generations within my family and has its roots in the actions of my greedy mercenary anchestor Kyle Lyonsbane, who during a battle abandoned his wounded companion, a sorceress, on the battlefield to die so that he could plunder an enemy's stronghold. As vengeance the sorceress cursed Kyle and our family. ~
+=~We transform into a werepanther under stressful circumstances, just like you witnessed.~
+==CVSan25J~It has happened to you before (You take his hand), am I right?~
+==ToKelem~It happened when my father assaulted Lilianna, just like he did with Tannith years before that. They were the lights of my youth and he destroyed them. I was too weak against him to prevent it.~
+==TOPellig~You were not too weak in the animal form.~
+==ToKelem~I killed my father - and now I killed my brother.~
+END
+++~(You take him in your arms and soothe him wordlessly.)~EXIT
+++~You are strong now, strong enough not to let it happen again - not to me.~EXIT
+++~You are not alone anymore, you and me, we are strong together.~EXIT
